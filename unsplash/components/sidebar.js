200c84e1e155024e6cb2715512c87fb2a672021b
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-02-21 12:50:34 +0530

    Add files via upload

---
+++ unsplash/components/sidebar.js
@@ -0,0 +1,14 @@
+//return nav bar
+function sideBar(){
+    return `
+    <div>MENU</div>
+    <div>LOGIN</div>
+    <div>SIGNUP</div>
+    <div>NEWS</div>
+    <div>API</div>
+    <input type="text" id="searchbar">
+    
+    `
+}
+
+export default sideBar
